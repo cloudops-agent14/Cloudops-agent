e27b6e6d2d80545f2232eb41814ee22c168fe8be
cloudops-agent14 <cloudopsbot@gmail.com>
2025-09-05 11:58:08 -0400

    Update cloudops_bot_ui.py

--- cloudops_bot_ui.py
+++ cloudops_bot_ui.py
@@ -32,6 +32,7 @@ st.markdown(
         box-shadow: 2px 2px 10px rgba(0,0,0,0.1);
     ">
         <h3 style="color:#2E7D32;">🌟 I am your COMFY ! Your AI Cloud-ops Assistant</h3>
+        <h2 style="color:#2E7D32;">🌟 Your AI Cloud-ops Assistant</h2>
         <p>I am <b>COMFY</b> - Who am I & What can I do? I can:</p>
         <ul>
             <li>💰 Provide you with <b>cost optimization solutions</b> for your AWS account</li>
@@ -116,6 +117,7 @@ if st.session_state["pending_query"]:
         st.session_state["pending_query"] = None
         status.update(label="✅ Response received!", state="complete", expanded=False)
         st.rerun()
+
 
 
 
